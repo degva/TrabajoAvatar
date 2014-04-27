7d9df4902c33935f9bdbf35ec0cde96ef44fd1ce
Diego Vargas <degva.29@gmail.com>
2014-04-27 09:47:58 -0500

    Agrege algo de animacion tambien

--- js/animation.js
+++ js/animation.js
@@ -1,13 +1,24 @@
 $(document).ready( function() {
+
 	/* Esta es la funcion para correr la peli
 	 * Jojo
 	 */
 	console.log('Comenzando movimiento...');
-	setTimeout( function() {
-		// move5('#smallBox', 'top', 20);
-		$('#smallBox').animate({'top': "+=20px"}, 'slow');
 
+	setTimeout( function() {
+		for (i = 0; i < 10; i++) {
+			$('#smallBox').animate({'top': "+=20px"}, 'fast');
+			$('#smallBox').animate({'left': "+=20px"}, 'fast');
+		}
 	}, 2000);
+
+	setTimeout( function() {
+		for (i = 0; i < 10; i++) {
+			$('#smallBox2').animate({'top': "+=20px"}, 'fast');
+			$('#smallBox2').animate({'left': "-=20px"}, 'fast');
+		}
+	}, 2000);
+
 	console.log('Movido 10px hacia abajo D:');
 	
 });
