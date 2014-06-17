a83fa6095f9c4bf507f5e221ff564a1f69dc8d95
Carlos <tearlach_159@live.com>
2014-06-17 18:18:42 -0500

    funcion mostrar texto
    con puntos-y-comas

--- funcion mostrar texto.js
+++ funcion mostrar texto.js
@@ -1,7 +1,7 @@
 function mostrartexto(textocuy) { 
-    var texto
-    texto.src="textocuy.txt"
-    var canvas=document.getElementById('canvastexto')
+    var texto;
+    texto.src="textocuy.txt";
+    var canvas=document.getElementById('canvastexto');
      var context = canvas.getContext('2d');
 
       context.font = 'italic 20pt Calibri';
